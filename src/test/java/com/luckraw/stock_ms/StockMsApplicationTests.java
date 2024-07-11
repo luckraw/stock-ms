c4561687845385f94c8279ada1a86e5702fb9a90
Lucas <krawdev@gmail.com>
2024-07-11 12:23:29 -0300

    feat: project init

---
+++ src/test/java/com/luckraw/stock_ms/StockMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckraw.stock_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
