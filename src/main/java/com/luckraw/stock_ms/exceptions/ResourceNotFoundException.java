892eaf9587d7e865a4e8a5ce60c20db62af62093
Lucas <krawdev@gmail.com>
2024-07-12 11:58:35 -0300

    feat: Handler/Custom Exception impl.

---
+++ src/main/java/com/luckraw/stock_ms/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.luckraw.stock_ms.exceptions;
+
+public class ResourceNotFoundException extends Exception{
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
